c8ddf330231c0ce545dadb85fbe41c45ee9344b7
wren gayle romano <wren@community.haskell.org>
2012-01-28 23:22:09 -0500

    Some cleanup for releasing
    Ignore-this: 75f5d99574ba9457d49b4e9bdca42688
    
    darcs-hash:fa001628a219fb4d93daed085b3cfff726b7b921

--- src/Data/Function/Pointless.hs
+++ src/Data/Function/Pointless.hs
@@ -3,7 +3,7 @@
 --                                                    2012.01.06
 -- |
 -- Module      :  Data.Function.Pointless
--- Copyright   :  Copyright (c) 2009--2011 wren ng thornton
+-- Copyright   :  Copyright (c) 2009--2012 wren ng thornton
 -- License     :  BSD
 -- Maintainer  :  wren@community.haskell.org
 -- Stability   :  provisional
